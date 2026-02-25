43d084de03732c1084f823d4df566cdb5ed03fc5
Xingguang822 <213233495@seu.edu.cn>
2026-02-25 09:13:08 +0800

    HTTP协议请求数据的获取

---
+++ springboot-web-quickstart/src/main/java/org/example/RequestController.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+
+    @RequestMapping("/request")
+    public String request(HttpServletRequest request){
+        String method = request.getMethod();
+        System.out.println("method:"+method);
+
+        String url = request.getRequestURL().toString();
+        System.out.println("url:"+url);
+        String uri = request.getRequestURI().toString();
+        System.out.println("uri:"+uri);
+
+        String protocol = request.getProtocol();
+        System.out.println("protocol:"+protocol);
+
+        String name=request.getParameter("name");
+        System.out.println("name:"+name);
+
+        String accept = request.getHeader("accept");
+        System.out.println("accept:"+accept);
+
+        return "ok";
+    }
+}
