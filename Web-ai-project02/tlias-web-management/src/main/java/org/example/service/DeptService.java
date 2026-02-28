819f4ef02ec8e4db95460471cca5633c1fb3c609
Xingguang822 <213233495@seu.edu.cn>
2026-02-28 23:26:09 +0800

    tlias项目部门管理的删除新增和修改功能

--- Web-ai-project02/tlias-web-management/src/main/java/org/example/service/DeptService.java
+++ Web-ai-project02/tlias-web-management/src/main/java/org/example/service/DeptService.java
@@ -6,5 +6,14 @@ import java.util.List;
 
 
 public interface DeptService {
+
     List<Dept> findall();
+
+    void deleteByID(Integer id);
+
+    void add(Dept dept);
+
+    Dept getById(Integer id);
+
+    void update(Dept dept);
 }
