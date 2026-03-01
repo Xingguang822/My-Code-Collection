08660fea836e7f3f8a8c49badfd22bbb9fcb51a4
Xingguang822 <213233495@seu.edu.cn>
2026-03-01 11:11:20 +0800

    tlias项目员工管理的准备工作

---
+++ Web-ai-project02/tlias-web-management/src/main/java/org/example/controller/EmpController.java
@@ -0,0 +1,9 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class EmpController {
+}
---
+++ Web-ai-project02/tlias-web-management/src/main/java/org/example/service/impl/EmpSeaviceImpl.java
@@ -0,0 +1,8 @@
+package org.example.service.impl;
+
+import org.example.service.EmpService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpSeaviceImpl implements EmpService {
+}
